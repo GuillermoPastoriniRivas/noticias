288caad2c7eb06bdd106655274f268fc7634d32d
GuillermoPastoriniRivas <guillepastorini@hotmail.com>
2020-02-28 16:49:56 -0300

    Updates

---
+++ precache-manifest.2d64ba096c17ac8e02e0cb6c23bc0b48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d3ca40a6db80423ba43b26c8697934",
+    "url": "/guillermopastorinirivas.github.io/noticias/index.html"
+  },
+  {
+    "revision": "40e20982623e0ecd3742",
+    "url": "/guillermopastorinirivas.github.io/noticias/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "3c9f04189b55ba446ebd",
+    "url": "/guillermopastorinirivas.github.io/noticias/static/js/2.c2caf1a5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/guillermopastorinirivas.github.io/noticias/static/js/2.c2caf1a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40e20982623e0ecd3742",
+    "url": "/guillermopastorinirivas.github.io/noticias/static/js/main.ba0815fd.chunk.js"
+  },
+  {
+    "revision": "2379e908f157b9abbf4e",
+    "url": "/guillermopastorinirivas.github.io/noticias/static/js/runtime-main.95d20f03.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/guillermopastorinirivas.github.io/noticias/static/media/logo.5d5d9eef.svg"
+  }
+]);
